5b6859511c9c48766cebfe0f81c1518f010f534c
Tri-via <turnerblackjack@gmail.com>
2023-02-18 12:43:55 +0530

    store all basic functionalities added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Cart } from './models/cart.models';
+import { CartService } from './services/cart.service';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  template: `
+    <app-header [cart]="cart"></app-header>
+    <router-outlet></router-outlet>
+  `,
+  styleUrls: [],
 })
-export class AppComponent {
-  title = 'store';
+export class AppComponent implements OnInit {
+  cart: Cart = { items: [] };
+
+  constructor(private cartService: CartService) {}
+
+  ngOnInit(): void {
+    this.cartService.cart.subscribe((_cart) => {
+      this.cart = _cart;
+    });
+  }
 }
